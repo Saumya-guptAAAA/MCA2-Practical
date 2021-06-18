8147e40020a842e2851a21e1742d158e0fe1072f
Saumya-guptAAAA <86101929+Saumya-guptAAAA@users.noreply.github.com>
2021-06-18 14:17:22 +0530

    Add files via upload

---
+++ MCA-2B/SaumyaGupta_20712024/q1/factorial.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Fact{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number :");
+		int n=sc.nextInt();
+		int f=1;
+		for(int i=1;i<=n;i++)
+			f=f*i;
+		System.out.println("Factorial of "+n+" is "+f);
+	}
+}
