ed3e5bd13d310f2291f8f024f70f6f3cd480f108
Saumya-guptAAAA <86101929+Saumya-guptAAAA@users.noreply.github.com>
2021-06-18 14:18:28 +0530

    Add files via upload

---
+++ MCA-2B/SaumyaGupta_20712024/q2/sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class summ
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter first number :");
+		int n=sc.nextInt();
+		System.out.println("Enter second number :");
+		int n2=sc.nextInt();
+		System.out.println("Enter third number :");
+		int n3=sc.nextInt();
+		int sum=n+n2+n3;
+		System.out.println("Sum of three numbers are :"+sum);
+	}
+}
